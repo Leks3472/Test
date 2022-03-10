815533474578ef92575a7a631605ecbe2562b461
Leks3472 <ras.ufa@mail.ru>
2022-03-11 02:10:28 +0500

    Тестовый калькулятор //в одном окне//

--- src/com/leks/CalculatorSimple.java
+++ src/com/leks/CalculatorSimple.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class CalculatorSimple {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         System.out.println("Введите выражение");
         Scanner scan = new Scanner(System.in);
         var expr = scan.nextLine();
